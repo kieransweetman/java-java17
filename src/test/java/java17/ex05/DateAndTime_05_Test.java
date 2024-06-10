0ae2e916899cec25c5f0ba1f9cd53ef46150860f
kieransweetman <kikisweetman@gmail.com>
2024-06-10 12:11:46 +0200

    feat: Add date and time tests for Java 17
    - Update DateAndTime_06_Test to create LocalDate instances
    - Update DateAndTime_07_Test to transform Java date to Instant and LocalDate
    - Update DateAndTime_01_Test to modify date and calendar objects
    - Update DateAndTime_05_Test to create LocalTime instances
    - Update DateAndTime_03_Test to create LocalTime instances using different methods

--- src/test/java/java17/ex05/DateAndTime_05_Test.java
+++ src/test/java/java17/ex05/DateAndTime_05_Test.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.time.Duration;
 import java.time.LocalTime;
-
+import java.time.format.DateTimeFormatter;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
@@ -18,13 +18,14 @@ public class DateAndTime_05_Test {
     public void test_duration() throws Exception {
 
         // TODO créer une heure à 12h30
-        LocalTime time1 = null;
+        LocalTime time1 = LocalTime.parse("12:30", DateTimeFormatter.ofPattern("HH:mm"));
 
         // TODO créer une heure à 16h32
-        LocalTime time2 = null;
+        LocalTime time2 = LocalTime.parse("16:32", DateTimeFormatter.ofPattern("HH:mm"));
 
-        // TODO créer une durée (classe java.time.Duration) qui représente le temps entre les heures précédentes
-        Duration duration = null;
+        // TODO créer une durée (classe java.time.Duration) qui représente le temps
+        // entre les heures précédentes
+        Duration duration = Duration.between(time1, time2);
 
         assertThat(duration.toMinutes(), is(242L));
         assertThat(duration.toHours(), is(4L));
@@ -35,6 +36,5 @@ public class DateAndTime_05_Test {
     }
 
     // Duration
-
 
 }
